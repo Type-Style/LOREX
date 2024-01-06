e7956f943f9f1bc5c225190759309660e0d9607b
Type-Style <github@type-style.de>
2024-01-06 20:20:00 +0100

    [Task] #3 nodemon

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import { Request, Response } from 'express';
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello TypeScript and Node.js!');
+  res.send('Hello World, via TypeScript and Node.js!');
 });
 
 app.listen(port, () => {
