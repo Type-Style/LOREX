63b352543670021a7ba02e47fd0b0853c663e8e1
Type-Style <github@type-style.de>
2024-01-12 18:32:29 +0100

    [CHANGE] #6 revert back to require output for production

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path  from 'path';
 
 const app = express();
 const port = 80;
@@ -16,7 +12,8 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(port, () => {
   const date = new Date().toLocaleString('de-DE', { hour12: false });
-  const logPath = join(__dirname, 'httpdocs', 'log.txt');
+  const logPath = path.join(__dirname, 'httpdocs', 'log.txt');
   fs.appendFileSync(logPath, `Express: Server:  ${date} \n`);
+  
   console.log(`Server läuft unter http://localhost:${port}`);  
 });
