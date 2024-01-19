4b00e07bc2162ecaf6f3df267b12ee03a2db746d
Type-Style <github@type-style.de>
2024-01-19 16:28:52 +0100

    [Temp, Task] #10 Validate inputs using express-validator and custom functions

--- src/controller/write.ts
+++ src/controller/write.ts
@@ -1,13 +1,22 @@
 import express from 'express';
-import { Request, Response } from 'express';
 import entry from '@src/models/entry';
+import { validationResult } from 'express-validator';
+
 
 const router = express.Router();
+router.use(entry.validate);
 
-// example call: /write?&user=xxx&lat=00.000&lon=00.000&timestamp=1704063600000&hdop=0.0&altitude=0.000&speed=0.000&heading=000.0
-router.get('/', (req:Request, res:Response) => {
-  entry.create(req, res);
-  console.log(req.query);
+// example call: /write?&user=xx&lat=00.000&lon=00.000&timestamp=1704063600000&hdop=0.0&altitude=0.000&speed=0.000&heading=000.0
+router.get('/', (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return next();		
+  }
+
+  
+  //entry.create(req, res);
+  //const test = process.env.TEST;
 
   res.send(`Write: ${JSON.stringify(req.query)}`);
 
