2363c0621f91ea488499c78869815401b58f9830
Type-Style <github@type-style.de>
2024-01-24 11:01:33 +0100

    [Fix] #35 test know import path structure now

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
 };
