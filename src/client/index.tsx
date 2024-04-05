fbb9315a7df5b7da6ebd70b2da00422252e843a1
Type-Style <github@type-style.de>
2024-04-05 08:50:50 +0200

    [Task] #58, webpack and react setup

---
+++ src/client/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>Hello, React!</h1>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
