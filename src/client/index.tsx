d65da31521e19b8766447ff3c0e69ec674421ad8
Type-Style <github@type-style.de>
2024-04-05 13:36:34 +0200

    58 react setup (#59)
    * [Task] #58 install react via npm, incl. types and eslint plugins
    
    * [Task] #58, tsconfig for react folder
    
    * [Task] #58 esLint config
    
    * [Task] #58, webpack and react setup
    
    * [Task] #58, render welcome from express instead of static
    
    * [Task] #58 eslint scripts
    
    * [Task] #58, eslint react setup
    
    * [TASK] #58 integrate webpack in build and dev npm scripts

---
+++ src/client/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Root, createRoot } from 'react-dom/client';
+
+const App = () => {
+  return (
+    <h1>Hello, React!</h1>
+  );
+};
+
+const container = document.getElementById('root');
+let root:Root;
+if (container) {
+  root = createRoot(container);
+  root.render(<App/>);
+} else {
+  console.error("root not found");
+}
