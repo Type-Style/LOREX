062f84cdbca605fc6a2e24f46e829fa1b85db50c
Type-Style <github@type-style.de>
2024-04-19 14:20:53 +0200

    [Task] #58 apply wrapper component

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Contact from './Contact';
-import  * as css from"./css/app.module.css";
+import Contacts from './Contacts';
+import * as css from "./css/app.module.css";
 
 class App extends Component {
   render() {
     return (
       <div className={css.app}>
         <h1 className={css.headline}>Hello, React!</h1>
-        <Contact name="Joe Doe" email='jd@gmail.com' phone='0123456789'/>
+        <Contacts />
       </div>
     );
   }
