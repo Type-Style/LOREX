2bf0d91fc27d53c24942adb2d88ef1291961870a
Type-Style <github@type-style.de>
2024-05-07 11:11:37 +0200

    [Task] #58, setup react router

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import { Root, createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/App";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+]);
+
 const container = document.getElementById('react-root');
-let root:Root;
+let root: Root;
 if (container) {
   root = createRoot(container);
-  root.render(<App/>);
+  root.render(
+    <RouterProvider router={router} />
+  );
 } else {
   console.error("root not found");
 }
+
+
