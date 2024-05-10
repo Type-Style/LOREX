f46941dfc5f3f58b5295399ceb9682f645685b2a
Type-Style <github@type-style.de>
2024-05-10 13:12:02 +0200

    [Temp] #61 introduce darkmode to MUI

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Root, createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Experimental_CssVarsProvider as CssVarsProvider, experimental_extendTheme as extendTheme, useColorScheme, getInitColorSchemeScript } from '@mui/material/styles';
 import App from "./components/App";
 
 const router = createBrowserRouter([
@@ -10,12 +11,34 @@ const router = createBrowserRouter([
   }
 ]);
 
+
+const theme = extendTheme({ // color pallette overwritten in css
+  typography: {
+    fontFamily: "Science-Gothic, sans-serif",
+    fontSize: 20,
+  },
+});
+
+let darkModeEnabled = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+function setMode(dark:boolean) {
+  darkModeEnabled = dark;
+  document.documentElement.dataset.muiColorScheme = darkModeEnabled ? "dark" : "light";
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+  setMode(event.matches);
+});
+
+
 const container = document.getElementById('react-root');
 let root: Root;
 if (container) {
+  setMode(darkModeEnabled);
   root = createRoot(container);
   root.render(
-    <RouterProvider router={router} />
+    <CssVarsProvider theme={theme}>
+      <RouterProvider router={router} />
+    </CssVarsProvider>
   );
 } else {
   console.error("root not found");
