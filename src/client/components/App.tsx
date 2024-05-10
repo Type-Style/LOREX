9c9a8c87111c2a7749bf6daf146a6a8467297a6b
Type-Style <github@type-style.de>
2024-05-10 15:50:24 +0200

    [Task] #61, move router to root App

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import "./css/app.css";
-import { Button, Typography } from '@mui/material';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useColorScheme } from '@mui/material/styles';
+import { useMediaQuery } from '@mui/material';
+import Start from './Start';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Start />,
+  }
+]);
 
 const App = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const { mode, setMode } = useColorScheme();
+  setMode(prefersDarkMode ? "dark" : "light");
+
   return (
-    <div className="app">
-      <h1 className="headline">Hello, React!</h1>
-      <Typography variant='h1'>Test Headline</Typography>
-      <Button variant='contained' size='large'>Test</Button>
-      <Button variant='contained' size='large' disabled={true}>Test2</Button>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,43 +1,22 @@
 import * as React from 'react';
 import { Root, createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Experimental_CssVarsProvider as CssVarsProvider, experimental_extendTheme as extendTheme, useColorScheme, getInitColorSchemeScript } from '@mui/material/styles';
+import { Experimental_CssVarsProvider as CssVarsProvider, experimental_extendTheme as extendTheme} from '@mui/material/styles';
 import App from "./components/App";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  }
-]);
-
 
 const theme = extendTheme({ // color pallette overwritten in css
   typography: {
     fontFamily: "Science-Gothic, sans-serif",
     fontSize: 20,
-  },
+  }
 });
-
-let darkModeEnabled = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-function setMode(dark:boolean) {
-  darkModeEnabled = dark;
-  document.documentElement.dataset.muiColorScheme = darkModeEnabled ? "dark" : "light";
-}
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-  setMode(event.matches);
-});
-
 
 const container = document.getElementById('react-root');
 let root: Root;
 if (container) {
-  setMode(darkModeEnabled);
   root = createRoot(container);
   root.render(
     <CssVarsProvider theme={theme}>
-      <RouterProvider router={router} />
+      <App />
     </CssVarsProvider>
   );
 } else {
