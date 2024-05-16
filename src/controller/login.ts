6879f6526c8c4e415aa0d48abb8e3efcbc8f24ad
Type-Style <github@type-style.de>
2024-05-16 17:18:39 +0200

    [Temp] #61, login controller commented out unused route, TO BE REFACTORED

--- src/controller/login.ts
+++ src/controller/login.ts
@@ -7,13 +7,14 @@ import { createJWT, createCSRF, validateCSRF } from '@src/scripts/token';
 
 const router = express.Router();
 
-router.get("/", baseSlowDown, baseRateLimiter, async function login(req: Request, res: Response, next: NextFunction) {
-  loginLimiter(req, res, () => {
-    const csrfToken = createCSRF(res, next);
-    res.locals = {...res.locals, text: 'start', csrfToken: csrfToken};
-    res.render("login-form");
-  });
-});
+// TODO refactor endpoint to get token
+// router.get("/", baseSlowDown, baseRateLimiter, async function login(req: Request, res: Response, next: NextFunction) {
+//   loginLimiter(req, res, () => {
+//     const csrfToken = createCSRF(res, next);
+//     res.locals = {...res.locals, text: 'start', csrfToken: csrfToken};
+//     res.render("login-form");
+//   });
+// });
 
 router.post("/", loginSlowDown, async function postLogin(req: Request, res: Response, next: NextFunction) {
   loginLimiter(req, res, async () => {
