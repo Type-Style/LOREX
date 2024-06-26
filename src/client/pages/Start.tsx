ef81273ecb8ad5bfe24d552ad9e28bbd3bdec1ca
Type-Style <github@type-style.de>
2024-06-26 17:00:01 +0200

    [Task] #77 1st draft layout

--- src/client/pages/Start.tsx
+++ src/client/pages/Start.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import { Button, Typography } from '@mui/material';
 import "../css/start.css";
 
 function Start() {
   return (
     <div className="start">
-      <h1 className="headline">Hello, React!!</h1>
-      <Typography variant='h1'>Test Headline </Typography>
-      <Button variant='contained' size='large'>Test</Button>
-      <Button variant='contained' size='large' disabled={true}>Test2</Button>
-
-      <svg className="bg-pattern" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100%" height="100%" fill="url(#repeatingGradient)" />
-      </svg>
+      <div className="grid-item info">info</div>
+      <div className="grid-item map">map</div>
+      <div className="grid-item status">status</div>
+      <div className="grid-item image">image1</div>
+      <div className="grid-item image">image2</div>
+      <div className="grid-item subinfo">subinfo</div>
     </div>
   )
 }
