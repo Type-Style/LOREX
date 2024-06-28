8c548bda2907dec763f99121e867482d8d31a83d
Type-Style <github@type-style.de>
2024-06-28 13:17:03 +0200

    [Temp] #77, log out data on valid request, temp: error handling and display

--- src/client/pages/Start.tsx
+++ src/client/pages/Start.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../css/start.css";
+import axios from 'axios';
 
 function Start() {
+  const [entries, setEntries] = useState<Models.IEntry[]>([]);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("jwt");
+    let response;
+
+    const getData = async () => {
+      try {
+        response = await axios({
+          method: 'get',
+          url: "/read?index=0",
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+        setEntries(response.data.entries);
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    
+    getData(); 
+    console.log(response);
+    
+    return () => {
+      console.log("cleanup")
+    };
+    
+  }, []);
   return (
     <div className="start">
-      <div className="grid-item info">info</div>
+      <div className="grid-item info">info: {JSON.stringify(entries)}</div>
       <div className="grid-item map">map</div>
       <div className="grid-item status">status</div>
       <div className="grid-item image">image1</div>
