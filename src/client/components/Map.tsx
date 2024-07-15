d8e6eec47cdbb629f2e4bbe6df96c2009233bd83
Type-Style <github@type-style.de>
2024-07-15 16:36:58 +0200

    [Task] #77 move map to new location

--- src/client/components/Map.tsx
+++ src/client/components/Map.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import React, { useEffect } from 'react'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 // import L from 'leaflet';
 import 'leaflet-defaulticon-compatibility';
 import * as css from "../css/map.module.css";
+
+
+// Used to recenter the map to new coordinates
+const MapRecenter= ({ lat, lng, zoomLevel }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    // Fly to that coordinates and set new zoom level
+    map.flyTo([lat, lng], zoomLevel );
+  }, [lat, lng]);
+  return null;
+
+};
 
 function Map({ entries }: { entries: Models.IEntry[] }) {
 	if (!entries?.length) {
@@ -14,6 +27,7 @@ function Map({ entries }: { entries: Models.IEntry[] }) {
 
 	return (
 		<MapContainer className={css.mapContainer} center={[lastEntry.lat, lastEntry.lon]} zoom={13} scrollWheelZoom={false}>
+			<MapRecenter lat={lastEntry.lat} lng={lastEntry.lon} zoomLevel={13} />
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
