f8f37ca5fdd28040d1e40f4e4e20c543b4c31d7a
Type-Style <github@type-style.de>
2024-08-19 20:09:32 +0200

    [Task] #94, marker

--- src/client/components/Map.tsx
+++ src/client/components/Map.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
 import { MapContainer, Marker, Polyline, Popup, TileLayer, useMap } from 'react-leaflet'
+import L, { Icon, IconOptions } from 'leaflet';
+import 'leaflet-rotatedmarker';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
-// import L from 'leaflet';
-import 'leaflet-defaulticon-compatibility';
 import "../css/map.css";
 import { LatLngExpression } from "leaflet";
+
 
 
 // Used to recenter the map to new coordinates
@@ -30,6 +30,37 @@ function Map({ entries }: { entries: Models.IEntry[] }) {
 
 	const polyline: LatLngExpression[] = cleanEntries.map((entry) => ([entry.lat, entry.lon]));
 
+
+	// Function to create custom icon with dynamic className
+	function createCustomIcon(entry: Models.IEntry) {
+		let className = "";
+		let iconSize = 15;
+		if (entry.index == 0) {
+			className = "start"
+		}
+		if (entry == lastEntry) {
+			className = "end"
+		}
+
+		if (className) {
+			iconSize = 22;
+		}
+
+		return L.divIcon({
+			html: `
+			<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+				<title>Marker Arrow</title>
+				<polygon fill="var(--contrastText, currentColor)" points="50,0 100,100 0,100" />
+			</svg>`,
+			shadowUrl: null,
+			shadowSize: null,
+			shadowAnchor: null,
+			iconSize: [iconSize, iconSize],
+			iconAnchor: [iconSize/2, iconSize/2],
+			popupAnchor: [0, 0],
+			className: `customMarkerIcon ${className}`,
+		});
+	}
 	return (
 		<MapContainer className="mapContainer" center={[lastEntry.lat, lastEntry.lon]} zoom={13} scrollWheelZoom={false}>
 			<MapRecenter lat={lastEntry.lat} lon={lastEntry.lon} zoom={13} />
@@ -38,9 +69,14 @@ function Map({ entries }: { entries: Models.IEntry[] }) {
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
 			{cleanEntries.map((entry) => {
-				console.log(entry.index);
 				return (
-					<Marker key={entry.index} position={[entry.lat, entry.lon]}>
+					<Marker
+						key={entry.index}
+						position={[entry.lat, entry.lon]}
+						icon={createCustomIcon(entry)}
+						rotationAngle={entry.heading}
+						rotationOrigin="center"
+					>
 						<Popup>
 							<pre>{JSON.stringify(entry, null, 2)}</pre>
 						</Popup>
