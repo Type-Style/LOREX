7fb42c0415f73fe2b96d5a1e3447b9b75a6a2e7e
Type-Style <github@type-style.de>
2024-08-26 11:38:01 +0200

    [Task] #77, change timing to round up, so it "feels" more accurate

--- src/client/pages/Start.tsx
+++ src/client/pages/Start.tsx
@@ -17,22 +17,21 @@ function timeAgo(timestamp: number): string {
   const diffInSeconds = Math.floor((now - timestamp) / 1000);
 
   const seconds = diffInSeconds;
-  const minutes = Math.floor(diffInSeconds / 60);
-  const hours = Math.floor(diffInSeconds / 3600);
-  const days = Math.floor(diffInSeconds / 86400);
-  const weeks = Math.floor(diffInSeconds / 604800);
-  const months = Math.floor(diffInSeconds / 2592000);
-  const years = Math.floor(diffInSeconds / 31536000);
+  const minutes = Math.round(diffInSeconds / 60);
+  const hours = Math.round(diffInSeconds / 3600);
+  const days = Math.round(diffInSeconds / 86400);
+  const months = Math.round(diffInSeconds / 2592000);
+  const years = Math.round(diffInSeconds / 31536000);
 
-  if (seconds < 8) { return "Instant"; }
-  else if (seconds < 30) { return "Just now" }
-  else if (seconds < 60) { return "a moment ago" }
-  else if (minutes < 60) { return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`; }
-  else if (hours < 24) { return `${hours} ${hours === 1 ? "hour" : "hours"} ago`; }
-  else if (days < 7) { return `${days} ${days === 1 ? "day" : "days"} ago`; }
-  else if (weeks < 4) { return `${weeks} ${weeks === 1 ? "week" : "weeks"} ago`; }
-  else if (months < 12) { return `${months} ${months === 1 ? "month" : "months"} ago`; }
-  else { return `${years} ${years === 1 ? "year" : "years"} ago`; }
+  if (seconds < 8) return "Instant";
+  else if (seconds < 25) return "Just now";
+  else if (seconds < 50) return "a moment ago";
+  else if (minutes < 60) return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+  else if (hours < 24) return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+  else if (days < 30) return `${days} ${days === 1 ? "day" : "days"} ago`;
+  else if (months < 12) return `${months} ${months === 1 ? "month" : "months"} ago`;
+  else return `${years} ${years === 1 ? "year" : "years"} ago`;
+
 }
 
 function Start() {
@@ -80,7 +79,7 @@ function Start() {
           } else {
             mergedEntries = newEntries;
           }
-          
+
           index.current = mergedEntries.length;
 
           return mergedEntries;
