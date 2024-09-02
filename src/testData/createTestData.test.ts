59d05376263423649550797013ebf1ddc0c36063
Type-Style <github@type-style.de>
2024-09-02 17:49:41 +0200

    [Task] #123, add option to send testdata to production

--- src/testData/createTestData.test.ts
+++ src/testData/createTestData.test.ts
@@ -1,13 +1,20 @@
 import axios, { AxiosError } from 'axios';
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const MODE = process.env.MODE;
+const SERVER = MODE == "PROD" ? process.env.ROOT : 'http://localhost:80';
+const environmentKey =  process.env.KEYA as string;
+const key = MODE == "PROD" ?  Buffer.from(environmentKey, 'base64').toString('utf-8') : "test";
+console.log("Sending Test Data to: " + SERVER);
 
 async function callServer(timestamp = new Date().getTime(), query: string, expectStatus: number = 200, method: string = "HEAD") {
-  const url = new URL("http://localhost:80/write?");
+  const url = new URL(`${SERVER}/write?`);
   url.search = "?" + query;
   const params = new URLSearchParams(url.search);
   params.set("timestamp", timestamp.toString());
   url.search = params.toString();
-
 
   let response;
   if (expectStatus == 200) {
@@ -48,7 +55,7 @@ describe('test Data', () => {
         const lat = (start.lat + (diff.lat / (entries - 1) * i)).toFixed(8);
         const lon = (start.lon + (diff.lon / (entries - 1) * i)).toFixed(8);
         setTimeout(async () => {
-          await callServer(undefined, `user=xx&lat=${lat}&lon=${lon}&timestamp=R3Pl4C3&hdop=${Math.floor(Math.random() * 15) + 1}&altitude=${i+1}&speed=${39 + i*2.5}&heading=${262 + Math.floor(Math.random() * 20) - 10}&key=test`, 200, "GET");
+          await callServer(undefined, `user=xx&lat=${lat}&lon=${lon}&timestamp=R3Pl4C3&hdop=${Math.floor(Math.random() * 15) + 1}&altitude=${i+1}&speed=${39 + i*2.5}&heading=${262 + Math.floor(Math.random() * 20) - 10}&key=${key}`, 200, "GET");
           console.log("called server " + (i + 1) + "/" + entries);
 
         }, 1000 * 30 * i);
