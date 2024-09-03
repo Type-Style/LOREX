6e4be920379d8edc7af62ccc2eeee4ea140dd1bf
Type-Style <github@type-style.de>
2024-09-03 12:14:37 +0200

    [CHANGE] #84, writing key check to be simpler, introduced scripts for… (#135)
    * [CHANGE] #84, writing key check to be simpler, introduced scripts for setting up environment variables
    
    * fix: upgrade express-validator from 7.1.0 to 7.2.0 (#133)
    
    Snyk has created this PR to upgrade express-validator from 7.1.0 to 7.2.0.
    
    See this package in npm:
    express-validator
    
    See this project in Snyk:
    https://app.snyk.io/org/type-style/project/e2bcd002-cb74-409c-ba55-fb6349df1cbc?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * fix: upgrade @mui/material from 5.16.6 to 5.16.7 (#132)
    
    Snyk has created this PR to upgrade @mui/material from 5.16.6 to 5.16.7.
    
    See this package in npm:
    @mui/material
    
    See this project in Snyk:
    https://app.snyk.io/org/type-style/project/e2bcd002-cb74-409c-ba55-fb6349df1cbc?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * Create .env
    
    For easier setup, to follow readme installation guide
    
    * Update README.md Installation Guide
    
    * fix: upgrade @mui/icons-material from 5.16.5 to 5.16.7 (#134)
    
    Snyk has created this PR to upgrade @mui/icons-material from 5.16.5 to 5.16.7.
    
    See this package in npm:
    @mui/icons-material
    
    See this project in Snyk:
    https://app.snyk.io/org/type-style/project/e2bcd002-cb74-409c-ba55-fb6349df1cbc?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * [Task] #84, remove .env
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

---
+++ init/generateKey.js
@@ -0,0 +1,30 @@
+/*
+* Usage: open console run:  node init/generateKey.js
+* type desired key and hit enter
+* copy output to .env add a line starting with:
+* KEY=
+* directly followed by your output
+*/
+
+// Import required modules
+const readline = require('readline');
+
+// set up readline to read input from the console
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Prompt user for input
+rl.question('Enter the string to be encoded: ', (input) => {
+  // encode to escape special chars
+  const escapedString = encodeURIComponent(input);
+
+  // convert the escaped string to base64
+  const base64String = Buffer.from(escapedString).toString('base64');
+
+  // print the result
+  console.log('Base64 Encoded String:', base64String);
+
+  rl.close();
+});
