68c5101412c6c2cde074f5ef78edef309ad41dfb
Type-Style <github@type-style.de>
2024-09-20 13:10:02 +0200

    [Task] #167, refactor context

--- src/client/components/MiniMap.tsx
+++ src/client/components/MiniMap.tsx
@@ -5,7 +5,7 @@ import { Context } from "./App";
 
 export default function MiniMap({ layer, lastEntry, index }: client.MiniMapProps) {
 
-	const [, , , , , , , mapToken] = useContext(Context);
+	const [contextObj] = useContext(Context);
 
 	const replaceKeyword = "XXXREPLACEXXX";
 
@@ -30,7 +30,7 @@ export default function MiniMap({ layer, lastEntry, index }: client.MiniMapProps
 				<MapRecenter lat={lastEntry.lat} lon={lastEntry.lon} zoom={15} fly={false} />
 				<TileLayer
 					attribution={layer.attribution}
-					url={layer.url.includes(replaceKeyword) ? layer.url.replace(replaceKeyword, mapToken) : layer.url}
+					url={layer.url.includes(replaceKeyword) ? layer.url.replace(replaceKeyword, contextObj.mapToken) : layer.url}
 					tileSize={layer.size || 256}
 					zoomOffset={layer.zoomOffset || 0}
 				/>
