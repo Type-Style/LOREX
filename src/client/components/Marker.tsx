9f30198c947665f12a4894727b1fd12bc9ef43cf
Type-Style <github@type-style.de>
2025-02-12 22:56:57 +0100

    [feat] #214, animate marker

--- src/client/components/Marker.tsx
+++ src/client/components/Marker.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Marker as LeafletMarker, Popup } from 'react-leaflet';
 import { Icon } from "./Icon";
 
-export const Marker = (entry: Models.IEntry, iconObj: { className: string, iconSize: number }) =>{
+export const Marker = (entry: Models.IEntry, iconObj: { className: string, iconSize: number}) => {
 	return (
 		<LeafletMarker
 			key={entry.time.created}
 			position={[entry.lat, entry.lon]}
-			icon={Icon(iconObj)}
+			icon={Icon(iconObj, entry)}
 			rotationAngle={entry.heading}
 			rotationOrigin="center"
 		>
