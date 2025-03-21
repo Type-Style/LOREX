9f78fb9dea14693024e41c344e29813246092b1c
Type-Style <github@type-style.de>
2025-03-21 22:46:35 +0100

    [Task] dashed line for large upload time

--- src/client/components/MultiColorPolyline.tsx
+++ src/client/components/MultiColorPolyline.tsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { parse, Oklch, toGamut, formatCss } from "culori";
 import { Polyline } from "react-leaflet";
 import { getMaxSpeed } from "../scripts/maxSpeed";
+import { LatLngTuple } from "leaflet";
 
 export const MultiColorPolyline = ({ cleanEntries }: { cleanEntries: Array<Models.IEntry>; }) => {
 	const [useRelativeColors] = useState<boolean>(true); // TODO Change candidate; Use color in range to maximum speed, like from 0 to max, rather than fixed range
 	let maxSpeed = 0;
 	const startColor = parse('oklch(62.8% 0.2577 10)') as Oklch;
-	const calculateHue = (currentSpeed, maxSpeed, calcSpeed) => {
+	const calculateHue = (currentSpeed:number, maxSpeed:number, calcSpeed:number) => {
 		let speed = currentSpeed;0
 		if (calcSpeed > currentSpeed) {
 			speed = 2 * (currentSpeed * calcSpeed) / (currentSpeed + calcSpeed); // Harmonic Mean
@@ -46,10 +47,12 @@ export const MultiColorPolyline = ({ cleanEntries }: { cleanEntries: Array<Model
 
 		let strokeDashArray:string|undefined;
 
-		if (entry.time.diff && (entry.time.diff > 100 || entry.time.diff < 25)) { strokeDashArray = "4 8"; }
+		
+
+		if (entry.time.uploadDuration > 300 || entry.time.diff && (entry.time.diff > 100 || entry.time.diff < 25)) { strokeDashArray = "4 8"; }
 		return (<Polyline
 			key={entry.time.created * 1.1 + Math.random()} // random to force rerender while new data is incoming (maxSpeed might have changed)
-			positions={[[previousEntry.lat, previousEntry.lon], [entry.lat, entry.lon]]}
+			positions={pathPositions}
 			color={formatCss(correctedColor)}
 			weight={5}
 			dashArray={strokeDashArray}
